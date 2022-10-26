f531e9cbfe5cfbff2e12a98ba94c09bf6b38a016
GitOfAMN <ashleynickens@gmail.com>
2022-10-25 21:30:26 -0400

    oops

--- models/fruit.js
+++ models/fruit.js
@@ -2,11 +2,10 @@ const mongoose = require('mongoose')
 
 // Make a Schema - tells us what we cannot do (i.e cannot be missing a d name, color, etc)
 const fruitSchema = new mongoose.Schema({
-    name: { type: String, require: true },
-    color: { type: String, required: true },
-    readyToEat: Boolean
+  name: { type: String, require: true },
+  color: { type: String, required: true },
+  readyToEat: Boolean
 })
-
 
 // Make a Model from the Schema - gives us all the methods to find the 'fruit' in this case. This model allows us to do all the CRUD capability necessary to items inside of a collection. Let's us update docs in the collection.
 
---
+++ models/seed.js
@@ -0,0 +1,21 @@
+const db = require('./db')
+const Fruit = require('./fruit')
+
+// Dummy code below
+const starterFruits = [
+    {
+        name: "Apple", color: "red", readyToEat: true,
+    },
+    {
+        name: "Orange", color: "orange", readyToEat: true,
+    },
+    {
+        name: "Coconut", color: "brown", readyToEat: true,
+    },
+    {
+        name: "Grapes", color: "red", readyToEat: true,
+    },
+    {
+        name: "Pear", color: "green", readyToEat: false,
+    }
+]
